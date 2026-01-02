fd1c3832c1793fde4326c95b844b9e57c5a2000c
BladyBoy <22pa1a4214@vishnu.edu.in>
2026-01-02 21:44:46 +0530

    Fresh Design with robust code

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,7 +5,7 @@ const authController = require('../controllers/authController');
 router.get('/signup', authController.signupPage);
 router.post('/users', authController.createUser);
 router.get('/login', authController.loginPage);
-router.post('/session', authController.loginUser);
+router.post('/login', authController.loginUser);
 router.get('/signout', authController.signout);
 
 module.exports = router;
